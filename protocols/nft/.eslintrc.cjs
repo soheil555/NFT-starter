84a48c64beca96d16ea47a2bfbd45760a8b4d756
soheil555 <soheilrasekh555@gmail.com>
2023-12-28 15:17:49 +0330

    feat: install svelte

--- protocols/nft/.eslintrc.cjs
+++ protocols/nft/.eslintrc.cjs
@@ -1,9 +1,13 @@
+/** @type { import("eslint").Linter.Config } */
 module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
   ],
+  env: {
+    node: true,
+  },
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   root: true,
